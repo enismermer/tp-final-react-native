53910eb60e3287d2140ec6ba5c5a7b0603f2f2c7
enismermer <enismermer38@gmail.com>
2025-06-25 09:53:01 +0200

    STEP THREE - Datas dans le cloud

--- app/index.tsx
+++ app/index.tsx
@@ -10,10 +10,26 @@ export default function Index() {
     setBatteryLevel((level * 100).toFixed(0));
   };
 
+  const sendBatteryToAPI = async () => {
+  const level = await Battery.getBatteryLevelAsync();
+  const payload = {
+    level: (level * 100).toFixed(0),
+    timestamp: new Date().toISOString()
+  };
+
+  await fetch('https://685ba7df89952852c2da65a5.mockapi.io/api/v1/battery', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload)
+  });
+};
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Button title="Afficher le niveau de batterie" onPress={getBatteryLevel} />
       {batteryLevel && <Text>Niveau de batterie : {batteryLevel}%</Text>}
+
+      <Button title="Envoyer à l'API" onPress={sendBatteryToAPI} />
     </View>
   );
 }
